c5d7b795aea44b28e3a5c6797f0e3769355ccb33
Ella <92597238+ellamiamaria@users.noreply.github.com>
2022-05-12 13:45:44 +0200

    Add files via upload

---
+++ app/src/main/java/de/buw/se4de/BookingSystem.java
@@ -0,0 +1,22 @@
+package de.buw.se4de;
+
+public class BookingSystem {
+
+	// main Method
+	public static void main(String[] args) {
+
+		Patient p = new Patient();
+		//Appointment a = new Appointment();
+		//a.setDoctor();
+		//a.setSpeciality();
+		//a.setDate();
+		//a.setTime();
+		//System.out.println("Dear " + p.getName() + "!" + " You have successfully booked an appointment with " + "Dr. "
+				//+ a.getDocName() + " on " + a.getDate() + " at " + a.getTime() + "!");
+	}
+
+	
+	// class Appointment for Doctors schedule
+	
+
+}
